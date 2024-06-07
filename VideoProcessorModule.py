f66069dbc61b87845923ec550e8b3794e20b00bb
Nicolas Vidal <vidalnicolasd@gmail.com>
2024-06-07 10:03:53 -0500

    Made the ResultSaver class be called inside the VideoPprocessor to keep this last class as a simple interface. New method save_results gets inyected with the ResultSaver instance.

--- VideoProcessorModule.py
+++ VideoProcessorModule.py
@@ -3,55 +3,15 @@ import cv2
 import time
 from PoseLandmarkDetectorModule import PoseDetector
 from LandmarkDatasetModule import VideoLandmarkDataSet
+from ResultSaverModule import ResultSaver
 
 class VideoProcessor:
-    """
-        A class used to process video frames for pose detection.
 
-        Attributes
-        ----------
-        video_name : str
-            The name of the video file.
-        video_display : bool
-            A flag indicating whether to display the video during processing.
-        video_path : str
-            The full path to the video file.
-        cap : cv2.VideoCapture
-            The video capture object.
-        detector : PoseDetector
-            The pose detector object.
-        pTime : float
-            The previous timestamp for FPS calculation.
-        frame : int
-            The current frame count.
-        videoDataSet : VideoLandmarkDataSet
-            The dataset for storing landmarks.
-
-        Methods
-        -------
-        _verify_video_path():
-            Verifies if the video file exists in the './videos' directory.
-        set_video_display(video_display: bool):
-            Sets the video display flag.
-        process_video():
-            Processes video frames, detects poses, optionally displays the video, and stores landmark data.
-        _write_fps(img: cv2.Mat, fps: float):
-            Writes the FPS value on the image.
-    """
     def __init__(self, video_name: str, video_display: bool = True) -> None:
-        """
-        Initializes the VideoProcessor with the specified video name and display option.
 
-        Parameters
-        ----------
-        video_name : str
-            The name of the video file.
-        video_display : bool, optional
-            A flag indicating whether to display the video during processing (default is True).
-        """
         self.video_name = video_name
         self.video_display = video_display
-        self._verify_video_path()
+        self.verify_video_path()
         self.video_path = os.path.join('./videos', self.video_name)
         self.cap = cv2.VideoCapture(self.video_path)
         self.detector = PoseDetector()
@@ -59,34 +19,18 @@ class VideoProcessor:
         self.frame = 0
         self.videoDataSet = VideoLandmarkDataSet(self.video_path)
 
-    def _verify_video_path(self) -> None:
-        """
-            Verifies if the video file exists in the './videos' directory.
-            
-            Raises
-            ------
-            FileNotFoundError
-                If the video file does not exist.
-        """
+    def verify_video_path(self) -> None:
+
         video_path = os.path.join('./videos', self.video_name)
         if not os.path.isfile(video_path):
             raise FileNotFoundError(f"The video file '{self.video_name}' does not exist in the './videos' directory.")
     
     def set_video_display(self, video_display: bool) -> None:
-        """
-            Sets the video display flag.
 
-            Parameters
-            ----------
-            video_display : bool
-                A flag indicating whether to display the video during processing.
-        """
         self.video_display = video_display
 
     def process_video(self) -> None:
-        """
-            Processes video frames, detects poses, optionally displays the video, and stores landmark data.
-        """
+
         success, img = self.cap.read()
 
         while success:
@@ -99,7 +43,7 @@ class VideoProcessor:
             if self.video_display:
                 cTime = time.time()
                 fps, self.pTime = self.detector.calculateFPS(self.pTime, cTime)
-                self._write_fps(img, fps)
+                self.write_fps(img, fps)
                 cv2.imshow("Image", img)
 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -112,15 +56,9 @@ class VideoProcessor:
         self.cap.release()
         cv2.destroyAllWindows()
 
-    def _write_fps(self, img: cv2.Mat, fps: float) -> None:
-        """
-            Writes the FPS value on the image.
-
-            Parameters
-            ----------
-            img : cv2.Mat
-                The image on which to write the FPS value.
-            fps : float
-                The frames per second value to write.
-        """
+    def write_fps(self, img: cv2.Mat, fps: float) -> None:
         cv2.putText(img, f'FPS: {int(fps)}', (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    def save_results(self, inyection_result_saver: ResultSaver = None) -> None:
+        instance_result_saver: ResultSaver = inyection_result_saver or ResultSaver()
+        instance_result_saver.save_results(self.video_name, self.videoDataSet)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from VideoProcessorModule import *
-from ResultSaverModule import *
+
 
 def main() -> None:
     """
@@ -8,9 +8,8 @@ def main() -> None:
     video_name = 'yoga1.mp4'
     video_processor = VideoProcessor(video_name=video_name)
     video_processor.process_video()
+    video_processor.save_results()
 
-    result_saver = ResultSaver(video_name=video_name, videoDataSet=video_processor.videoDataSet)
-    result_saver.save_results()
 
 if __name__ == "__main__":
     main()
